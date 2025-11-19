80c8e3708a4a80b566e9c28572e8b5971859f646
Aryan Malla <aryanmalla19@gmail.com>
2025-11-19 22:15:31 +0545

    added required migartions files

---
+++ database/migrations/2025_11_19_155205_create_matches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->string('status')->default('not_started');
+            $table->integer('halftime_duration')->nullable();
+            $table->boolean('is_halftime')->default(false);
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->integer('home_score')->default(0);
+            $table->integer('away_score')->default(0);
+            $table->foreignId('league_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('home_team_id')->constrained('teams')->cascadeOnDelete();
+            $table->foreignId('away_team_id')->constrained('teams')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
